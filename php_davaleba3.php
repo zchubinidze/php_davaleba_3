9a3ac2872662434e33ffc071383d6995a58f5cdf
zchubinidze <chubina1981zura@yahoo.com>
2024-02-02 02:20:16 +0400

    Create php_davaleba3.php

---
+++ php_davaleba3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "php დავალება3" ?></title>
+</head>
+<body>
+    <?php
+function myFunction($fname, $lname) {
+	echo $fname;
+}
+ myFunction("zurab", "chubinidze");
+    ?>
+</body>
+</html>
